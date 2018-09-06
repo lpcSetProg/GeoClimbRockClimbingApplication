0c75853062f17b901a666f955391c1ce805abba1
Carl Wilson <carlwilson@Carls-MacBook-Pro.local>
2018-09-06 15:05:14 -0400

    initial commit

---
+++ GeoClimb/GeoClimb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeoClimb
+//
+//  Created by Carl Wilson on 2018-09-06.
+//  Copyright © 2018 qualityoverload2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
