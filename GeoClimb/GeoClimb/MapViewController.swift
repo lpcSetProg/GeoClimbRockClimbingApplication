ac62547add1d4fa215350af283073d06020b7f15
LevyPaul <Lcocarell2816@conestogac.on.ca>
2018-12-07 18:03:32 -0500

    submitted version with comments and build changes
    before submitting to igor

--- GeoClimb/GeoClimb/MapViewController.swift
+++ GeoClimb/GeoClimb/MapViewController.swift
@@ -1,16 +1,15 @@
 /*
- * FILE : HomeViewController.swift
+ * FILE : MapViewController
  * PROJECT : PROG3230 - Mobile Application Development II - Assignment 1
  * PROGRAMMERS : David Pitters, Lev Cocarell, Carl Wilson, Bobby Vu
- * FIRST VERSION : 2018-09-15
+ * FIRST VERSION : 2018-12-03
  * DESCRIPTION :
- * This file contains the source code for the HomeViewController which currently acts as our home
- * page. This page is purely read only right now - but we plan on doing more with it in the future.
+ * This file contains the source code for the MapViewController which currently acts as our map feature. If the user clicks the show map button on home
+ * page. This page is purely read only right now.
  */
 
 import UIKit
 import MapKit
-
 
 
 class MapViewController: UIViewController {
@@ -18,23 +17,21 @@ class MapViewController: UIViewController {
     @IBOutlet weak var map: MKMapView!
     
    
-    
-    
-   
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // coordinates for map
         var centerLocation = CLLocationCoordinate2DMake(43.441997, -80.475146)
+        // span of coordinates
         var mapSpan = MKCoordinateSpanMake(0.1, 0.1)
        
+        // show map
         var mapRegion = MKCoordinateRegionMake(centerLocation, mapSpan)
         
+        // map annitation -- > shows pin for user
          let anni = MKPointAnnotation()
         anni.coordinate = centerLocation
         anni.title = "Grand River Rocks"
         anni.subtitle = "Local Gym"
-        
-        
         self.map.setRegion(mapRegion, animated: true)
         self.map.addAnnotation(anni)
         
